b3a9d2dfcfb46f451598b4b13e621af405ea5ca4
Vladyslav Novikov <vladyslav.novikov@netcracker.com>
2025-07-25 15:11:53 +0300

    chore: update dependencies and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@stoplight'],
   rules: {
+    'prettier/prettier': 'off',
     'no-console': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
@@ -10,5 +11,47 @@ module.exports = {
         patterns: ['lodash/*', '@fortawesome/free-solid-svg-icons/*'],
       },
     ],
+    'camelcase': 'error',
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'always-multiline',
+      },
+    ],
+    'eol-last': [
+      'error',
+      'always',
+    ],
+    'eqeqeq': [
+      'error',
+      'always',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
   },
+  overrides: [
+    {
+      files: ['src/__fixtures__/**/*'],
+      rules: {
+        camelcase: 'off',
+      },
+    },
+    {
+      files: ['**/*.json', '**/*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+      extends: [
+        'plugin:jsonc/recommended-with-json',
+      ],
+    },
+  ],
 }
