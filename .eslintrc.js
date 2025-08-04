738a8de6d45ad219fa444c82b977fcbffa756434
Vladyslav Novikov <vladyslav.novikov@netcracker.com>
2025-08-04 14:55:20 +0300

    chore: align react and react types versions and adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     'prettier/prettier': 'off',
     'no-console': 'off',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off', // disabled because it is not relevant for react 17+ ("jsx": "react-jsx")
     'no-restricted-imports': [
       'error',
       {
