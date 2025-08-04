5f962409bb74852f32a9d00fd15d4cf000381f8f
Iurii Golovinskii <iurii-golovinskii.qubership@gmail.com>
2025-08-04 11:09:11 +0300

    Configure Monaco bundling during REST playground build

--- src/components/Examples/ExampleViewer.tsx
+++ src/components/Examples/ExampleViewer.tsx
@@ -1,6 +1,10 @@
 import React, { FC, memo } from 'react'
 import { Typography } from '@mui/material'
-import { Editor } from '@monaco-editor/react'
+import * as monaco from 'monaco-editor';
+import Editor from '@monaco-editor/react';
+import { loader } from '@monaco-editor/react';
+
+loader.config({ monaco });
 
 export type ExampleViewerProps = {
   example: string;
