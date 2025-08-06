b9a0d7e23e8c214ca5998e0123a25f1a8ab2f9db
Iurii Golovinskii <iurii-golovinskii.qubership@gmail.com>
2025-08-06 16:40:54 +0300

    Test with another monaco version

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,5 @@ export { isHttpOperation, isHttpService } from './utils/guards'
 export * from './utils/ref-resolving'
 export { slugify } from './utils/string'
 export { createElementClass } from './web-components/createElementClass'
+
+__webpack_public_path__ = new URL('.', import.meta.url).toString();
